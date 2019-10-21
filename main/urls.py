5f13451ed762db503872e989e2bf9c71b7613583
Your Name <you@example.com>
2019-10-21 23:14:45 +0400

    commit

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,7 @@ urlpatterns = i18n_patterns(
 )
 
 
-# urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
